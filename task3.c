6e30e4f3da333dd293ae51a32cc281916b9efa39
swaggu <swagglordbm@gmail.com>
2023-02-08 22:48:29 +0000

    coming soon

---
+++ task3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <openssl/bn.h>
+#define NBITS 256
+void printBN(char *msg, BIGNUM *a)
+{
+/*Use BN_bn2hex(a) for hex string
+*Use BN_bn2dec(a) for decimal string*/
+char *number_str = BN_bn2hex(a);
+printf("%s %s\n", msg, number_str);
+OPENSSL_free(number_str);
+}
+int main ()
+{
+BN_CTX *ctx = BN_CTX_new();
+BIGNUM *m = BN_new();
+BIGNUM *n = BN_new();
+BIGNUM *d = BN_new();
+BIGNUM *e = BN_new();
+BIGNUM *res = BN_new();
+// Initialize a, b, n
+//BN_generate_prime_ex(a, NBITS, 1, NULL, NULL, NULL);
+//signing a messag means apply RSA function with the private key on message
+//S = R(m,d) = m^d mod n
+//message = I owe you $2000
+//message in hex = 49206f776520796f75202432303030
+BN_hex2bn(&m, "49206f776520796f75202432303030");
+BN_hex2bn(&n, 
+"DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A
+5");
+BN_hex2bn(&d, 
+"74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30
+D");
+BN_hex2bn(&e, "010001");
+//calculate res = m^d mod n and print it as s
+BN_mod_exp(res, m, d, n, ctx);
+printBN("s = ", res);
+//message = I owe you $3000
+//message in hex = 49206f776520796f75202433303030
+BN_hex2bn(&m, "49206f776520796f75202433303030");
+BN_hex2bn(&n, 
+"DCBFFE3E51F62E09CE7032E2677A78946A849DC4CDDE3A4D0CB81629242FB1A
+5");
+BN_hex2bn(&d, 
+"74D806F9F3A62BAE331FFE3F0A68AFE35B3D2E4794148AACBC26AA381CD7D30
+D");
+BN_hex2bn(&e, "010001");
+//calculate res = m^d mod n and print it as s
+BN_mod_exp(res, m, d, n, ctx);
+printBN("s = ", res);
+return 0;
+}
